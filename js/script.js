92f8789fdc163ae63ba9c1c47a458721825981c8
arzhk <72197750+arzhk@users.noreply.github.com>
2020-10-22 20:56:08 +0100

    Added functionality
    - Added tracklist table generation and population.
    
    - Updated styling for tables

--- js/script.js
+++ js/script.js
@@ -1,4 +1,171 @@
+const albumDB = [
+  {
+    albumName: "Album 1",
+    tracklist: [
+      "Center",
+      "Suspicion",
+      "Accept",
+      "Salvation",
+      "Regular",
+      "Border",
+      "Distortion",
+      "Gradual",
+      "Anticipation",
+      "Personal",
+      "Credibility",
+      "Pioneer",
+    ],
+  },
+  {
+    albumName: "Album 2",
+    tracklist: [
+      "Operational",
+      "Position",
+      "Governor",
+      "Opposition",
+      "Include",
+      "Precede",
+      "Obscure",
+      "Professor",
+      "Difference",
+      "Construct",
+    ],
+  },
+  {
+    albumName: "Album 3",
+    tracklist: [
+      "Traditional",
+      "Disastrous",
+      "Automatic",
+      "Gratis",
+      "Literate",
+      "Superficial",
+      "Curious",
+      "Miscreant",
+    ],
+  },
+  {
+    albumName: "Album 4",
+    tracklist: [
+      "Polite",
+      "Careless",
+      "Serious",
+      "Deserted",
+      "Changeable",
+      "Purple",
+      "Spurious",
+      "Spooky",
+      "Squeamish",
+      "Selective",
+    ],
+  },
+  {
+    albumName: "Album 5",
+    tracklist: [
+      "Center",
+      "Suspicion",
+      "Accept",
+      "Salvation",
+      "Regular",
+      "Border",
+      "Distortion",
+      "Pioneer",
+    ],
+  },
+  {
+    albumName: "Album 6",
+    tracklist: [
+      "Operational",
+      "Position",
+      "Governor",
+      "Opposition",
+      "Professor",
+      "Difference",
+      "Construct",
+    ],
+  },
+  {
+    albumName: "Album 7",
+    tracklist: [
+      "Traditional",
+      "Disastrous",
+      "Automatic",
+      "Gratis",
+      "Literate",
+      "Superficial",
+      "Strong",
+      "Squeamish",
+      "Miscreant",
+    ],
+  },
+  {
+    albumName: "Album 8",
+    tracklist: [
+      "Polite",
+      "Careless",
+      "Inquisitive",
+      "Historical",
+      "Serious",
+      "Deserted",
+      "Changeable",
+    ],
+  },
+  {
+    albumName: "Album 9",
+    tracklist: [
+      "Center",
+      "Suspicion",
+      "Distortion",
+      "Gradual",
+      "Anticipation",
+      "Personal",
+      "Credibility",
+      "Pioneer",
+    ],
+  },
+  {
+    albumName: "Album 10",
+    tracklist: [
+      "Operational",
+      "Position",
+      "Deteriorate",
+      "Formation",
+      "Difference",
+      "Construct",
+    ],
+  },
+  {
+    albumName: "Album 11",
+    tracklist: [
+      "Traditional",
+      "Automatic",
+      "Gratis",
+      "Superficial",
+      "Curious",
+      "Ambitious",
+      "Capricious",
+      "Squeamish",
+    ],
+  },
+  {
+    albumName: "Album 12",
+    tracklist: [
+      "Careless",
+      "Historical",
+      "Serious",
+      "Deserted",
+      "Changeable",
+      "Purple",
+      "Spurious",
+      "Squeamish",
+      "Selective",
+    ],
+  },
+];
+
 let activeLink = "";
+let allAlbums;
+let selectedAlbum;
 
 function navActive() {
   const navli = document.querySelectorAll(".nav-item");
@@ -20,9 +187,42 @@ function breadCrumb() {
     .join("")}</a>`;
 }
 
+function generateTableHead() {
+  const modalBody = document.querySelector(".modal-body");
+  const albumTable = document.createElement("table");
+  albumTable.classList.add("table", "table-dark");
+  const albumTableHead = document.createElement("thead");
+  const albumTableR = document.createElement("tr");
+
+  albumTableR.innerHTML =
+    `<th scope="col">#</th>` +
+    `<th scope="col">Track Title</th>` +
+    `<th scope="col" class="text-right">Track Length</th>`;
+
+  albumTableHead.appendChild(albumTableR);
+  albumTable.appendChild(albumTableHead);
+  modalBody.appendChild(albumTable);
+}
+
+function generateTableBody() {
+  const albumTable = document.querySelector("table");
+  const albumTableB = document.createElement("tbody");
+
+  for (let i = 1, j = 0; i < albumDB[selectedAlbum].tracklist.length + 1; i++) {
+    const albumTableNewRow = document.createElement("tr");
+    albumTableNewRow.innerHTML +=
+      `<th scope="row">${i}</th>` +
+      `<td>${albumDB[selectedAlbum].tracklist[i - 1]}</td>` +
+      `<td class="text-right">01:00:00</td>`;
+    albumTableB.appendChild(albumTableNewRow);
+    albumTable.appendChild(albumTableB);
+  }
+}
+
 function albumInfo() {
   const modalTitle = document.querySelector(".modal-title");
   const modalBody = document.querySelector(".modal-body");
+  allAlbums = document.querySelectorAll(".card");
 
   const parentCard = event.currentTarget.parentNode.querySelector("h5")
     .innerText;
@@ -30,14 +230,19 @@ function albumInfo() {
     "img"
   ).outerHTML;
 
+  allAlbums.forEach((e) => {
+    e.classList.remove("selected-album");
+  });
+  event.currentTarget.parentNode.parentNode.classList.add("selected-album");
+  for (let i = 0; i < allAlbums.length; i++) {
+    if (allAlbums[i].classList.contains("selected-album")) {
+      selectedAlbum = i;
+    }
+  }
+
   modalTitle.innerText = parentCard;
   modalBody.innerHTML = parentImg;
 
-  const newul = document.createElement("ul");
-  for (let i = 1; i < 13; i++) {
-    const newli = document.createElement("li");
-    newli.innerText = i + ". Track " + i;
-    newul.appendChild(newli);
-    modalBody.appendChild(newul);
-  }
+  generateTableHead();
+  generateTableBody();
 }
