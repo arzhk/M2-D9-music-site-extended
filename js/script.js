0bac0cfab24ba205da27438bf471be03f1eb7a03
arzhk <72197750+arzhk@users.noreply.github.com>
2020-10-22 16:01:12 +0100

    Added tracklist modal

--- js/script.js
+++ js/script.js
@@ -19,3 +19,25 @@ function breadCrumb() {
     .split(" ")
     .join("")}</a>`;
 }
+
+function albumInfo() {
+  const modalTitle = document.querySelector(".modal-title");
+  const modalBody = document.querySelector(".modal-body");
+
+  const parentCard = event.currentTarget.parentNode.querySelector("h5")
+    .innerText;
+  const parentImg = event.currentTarget.parentNode.parentNode.querySelector(
+    "img"
+  ).outerHTML;
+
+  modalTitle.innerText = parentCard;
+  modalBody.innerHTML = parentImg;
+
+  const newul = document.createElement("ul");
+  for (let i = 1; i < 13; i++) {
+    const newli = document.createElement("li");
+    newli.innerText = i + ". Track " + i;
+    newul.appendChild(newli);
+    modalBody.appendChild(newul);
+  }
+}
