90612ec89b5237e5129197ddbf00dbd418c6d1bc
arzhk <72197750+arzhk@users.noreply.github.com>
2020-10-22 14:25:16 +0100

    Added functionality
    - Working breadcrumbs
    - Working active nav function

--- js/script.js
+++ js/script.js
@@ -7,6 +7,15 @@ function navActive() {
   navLinks.forEach((e) => e.classList.remove("active"));
   event.target.classList.add("active");
   activeLink = event.target.innerText;
+  breadCrumb();
 }
 
-function breadCrumb() {}
+function breadCrumb() {
+  const currentBreadcrumb = document.querySelector(
+    ".breadcrumb-content-container .active"
+  );
+
+  currentBreadcrumb.innerHTML = `<a href=#${activeLink} class="white-txt">${activeLink
+    .split(" ")
+    .join("")}</a>`;
+}
